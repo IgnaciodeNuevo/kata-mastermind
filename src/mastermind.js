c742e6b82ea1eb6c4cde72b7a5a87b07e7daacae
Ignacio Villanueva <ivillanueva@plainconcepts.com>
2018-03-09 17:14:09 +0100

    Next iteration

--- src/mastermind.js
+++ src/mastermind.js
@@ -1,14 +1,20 @@
 module.exports = (arr1, arr2) => {
     let matchedColorAndIndex = 0;
     let matchedColor = 0;
+    let viewedColors = [];
+
     arr2.map((element, index) => {
-        if (arr1[index] != null && arr1[index] == element) {
-            matchedColorAndIndex++;
-        } else {
-            if (arr1.indexOf(element) != -1) {
-                matchedColor++;
+        if (viewedColors.indexOf(element) == -1) {
+            viewedColors.push(element);
+            if (arr1[index] != null && arr1[index] == element) {
+                matchedColorAndIndex++;
+            } else {
+                if (arr1.indexOf(element) != -1) {
+                    matchedColor++;
+                }
             }
         }
     });
-    return [matchedColorAndIndex, matchedColor];
+
+return [matchedColorAndIndex, matchedColor];
 };
