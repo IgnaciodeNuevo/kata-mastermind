e867b4545d06526c99492daeeb3e9027440700f7
Ignacio Villanueva <ivillanueva@plainconcepts.com>
2018-03-09 16:48:13 +0100

    Adrian

--- src/mastermind-test.js
+++ src/mastermind-test.js
@@ -6,6 +6,22 @@ describe("Mastermind", () => {
         arr1 = ['blue'];
         arr2 = ['red'];
         result = [0, 0];
-        Mastermind(arr1, arr2).should.eql(result);
+        Mastermind(arr1, arr2).should.deepEqual(result);
     });
+
+    it("Should match guessed color", () => {
+        arr1 = ['blue', 'red'];
+        arr2 = ['red', 'yellow'];
+        result = [0, 1];
+        Mastermind(arr1, arr2).should.deepEqual(result);
+    });
+
+    it("Should match color at right position", () => {
+        arr1 = ['red', 'blue'];
+        arr2 = ['red', 'orange'];
+        result = [1, 0];
+        Mastermind(arr1, arr2).should.deepEqual(result);
+    });
+
+
 });
--- src/mastermind.js
+++ src/mastermind.js
@@ -1,3 +1,15 @@
 module.exports = (arr1, arr2) => {
-    return null;
+    let matchedColorAndIndex = 0;
+    let matchedColor = 0;
+    arr1.map((element, index) => {
+        if (arr2[index] != null && arr2[index] == element) {
+            matchedColorAndIndex++;
+        } else {
+            if (arr2.indexOf(element) != -1) {
+                matchedColor++;
+            }
+        }
+
+    });
+    return [matchedColorAndIndex, matchedColor];
 };
